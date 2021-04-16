051a14fd9b39a4e5b5f004ebdc4e644cbe159fd9
Martha <martha.kandziora@yahoo.com>
2021-04-16 16:07:45 +0200

    fix update databases

--- update_databases.py
+++ update_databases.py
@@ -3,7 +3,7 @@ from PhylUp import db_updater, config
 configfi = "data/localblast.config"
 workdir = 'test/output/updt_db'
 
-conf = config.ConfigObj(configfi, workdir, interactive=True)
+conf = config.ConfigObj(configfi, workdir, interactive=False)
 
 db_updater._download_localblastdb(conf)
 db_updater._download_ncbi_parser(conf)
