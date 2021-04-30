61d4565bb8dc2a5a6af5f0353e2140c6dce7658f
Martha Kandziora <martha.kandziora@yahoo.com>
2021-04-30 13:47:40 +0200

    Update tests_setup.py

--- tests/tests_setup.py
+++ tests/tests_setup.py
@@ -1,9 +1,12 @@
 import os
-from shutil import copyfile
+from shutil import copyfile, rmtree
 
 
 src = "./data/data_for_tests/updt_aln.fasta"
 dst = "./tests/output/test_runs/updt_aln.fasta"
+if os.path.exists("./tests/output/"):
+    rmtree("./tests/output/")
+    
 os.mkdir("./tests/output/")
 os.mkdir("./tests/output/test_runs/")
 copyfile(src, dst)
